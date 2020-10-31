1796f20dd3fd74911e44a8f451ce5053e92ee3c8
Marco Cecchetti <marco.cecchetti@collabora.com>
2020-10-31 18:14:03 +0100

    Added configuration setting for Online server address

--- CollaboraRocketchatApp.ts
+++ CollaboraRocketchatApp.ts
@@ -1,7 +1,13 @@
-import { IAppAccessors, IConfigurationExtend, ILogger } from '@rocket.chat/apps-engine/definition/accessors';
+import {
+    IAppAccessors,
+    IConfigurationExtend,
+    IConfigurationModify, IHttp,
+    ILogger, IRead,
+} from '@rocket.chat/apps-engine/definition/accessors';
 import { ApiSecurity, ApiVisibility } from '@rocket.chat/apps-engine/definition/api';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { ISetting, SettingType } from '@rocket.chat/apps-engine/definition/settings';
 
 import { CheckFileInfoEndpoint } from './CheckFileInfoEndpoint';
 import { GetFileEndpoint } from './GetFileEndpoint';
@@ -13,11 +19,34 @@ export class CollaboraRocketchatApp extends App {
 
     public async extendConfiguration(configuration: IConfigurationExtend) {
         // Register API endpoints
-        configuration.api.provideApi({
+        await configuration.api.provideApi({
             visibility: ApiVisibility.PUBLIC,
             security: ApiSecurity.UNSECURE,
             endpoints: [new CheckFileInfoEndpoint(this), new GetFileEndpoint(this)],
         });
+
+        // Register Settings
+        await configuration.settings.provideSetting({
+            /** The id of this setting. */
+            id: 'OnlineServerUrl',
+            /** Type of setting this is. */
+            type: SettingType.STRING,
+            /** What is the default value (allows a reset button). */
+            packageValue: 'http://localhost',
+            /** Whether this setting is required or not. */
+            required: true,
+            /** Whether this setting is a public setting or not - administrators can see ones which are not public but users can't. */
+            public: false,
+            /** Name of the setting in the form of a i18n string. */
+            i18nLabel: 'Collabora_Online_Server',
+        });
     }
 
+    public async onSettingUpdated(setting: ISetting, configurationModify: IConfigurationModify, read: IRead, http: IHttp): Promise<void> {
+        console.log(`CollaboraRocketchatApp.onSettingUpdated: ${ JSON.stringify(setting) }`);
+        if (setting.id === 'OnlineServerUrl') {
+            const wopiAddress = setting.value || setting.packageValue;
+            console.log(`onSettingUpdated: wopiAddress: ${ wopiAddress }`);
+        }
+    }
 }
